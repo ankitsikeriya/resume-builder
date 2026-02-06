3afe36fbd6dbe2e6fa9dc2099959a8146f5eb2a9
Ankit sikeriya <156462765+ankitsikeriya@users.noreply.github.com>
2026-02-07 01:54:41 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config'
+import 'dotenv/config' 
 import { connectDB } from './config/db.js'; 
 import userRouter from './routes/userRouter.js'; 
 import resumeRouter from './routes/resumeRouter.js';
